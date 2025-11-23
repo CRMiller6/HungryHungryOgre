60eddeef13d937e313c15d1a3cb26ee6b2e3e931
Crow <crowmiller6@gmail.com>
2025-11-23 17:53:45 -0600

    cost value (not finished)

--- Assets/Scripts/ClickForScore.cs
+++ Assets/Scripts/ClickForScore.cs
@@ -8,20 +8,27 @@ public class ClickForScore : MonoBehaviour
     public float bugs = 0f;
     public TMP_Text bugsText;
     public GameObject bugsBox;
+
     public int ogrePow = 1;
     public TMP_Text ogrePowText;
     public GameObject ogrePowBox;
     public Button ogrePowButton;
+    public TMP_Text ogrePowButtText;
+
     public int minion = 0;
     public TMP_Text minionText;
     public GameObject minionBox;
     public Button minionButton;
+    public TMP_Text minionButtonText;
+
     public float second = 0.0f;
     public int secondInt = 1;
+
     public int village = 0;
     public TMP_Text villageText;
     public GameObject villageBox;
     public Button villageButton;
+    public TMP_Text villageButtonText;
     private float villagePower;
 
     //add villages at 10 oger pow
@@ -31,6 +38,9 @@ public class ClickForScore : MonoBehaviour
         ogrePowButton.interactable = false;
         minionButton.interactable = false;
         villageButton.interactable = false;
+        ogrePowButtText.text = "OGRE POWER \n" + "cost: " + ogrePow * 10 + " bugs";
+        minionButtonText.text = "MINION \n" + "cost: " + (100 * minion + 100) + " bugs";
+        //villageButtonText.text = "DESTROY VILLAGE \n" + "cost: 20 Ogre Power";
     }
 
     public void BugsOnClick()
@@ -58,8 +68,10 @@ public class ClickForScore : MonoBehaviour
 
             bugsText.text = "Bugs: " + bugs;
             ogrePowText.text = "Ogre Power: " + ogrePow;
+            ogrePowButtText.text = "OGRE POWER \n" + "cost: " + ogrePow * 10 + " bugs";
 
             villagePower = Mathf.Pow(village, village);
+            villageButtonText.text = "DESTROY VILLAGE \n" + "cost: EVERYTHING & minimum" + (villagePower * 10 + 10) + " Ogre Power";
 
             if (ogrePow >= villagePower * 10 + 10)
             {
@@ -87,6 +99,7 @@ public class ClickForScore : MonoBehaviour
             minion += 1;
             bugsText.text = "Bugs: " + bugs;
             minionText.text = "Minion: " + minion;
+            minionButtonText.text = "MINION \n" + "cost: " + (100 * minion + 100) + " bugs";
 
             if (bugs <= ogrePow * 10)
             {
@@ -111,10 +124,15 @@ public class ClickForScore : MonoBehaviour
             second -= 1;
             bugsText.text = "Bugs: " + bugs;
 
-            // if (bugs > 100 * minion + 100)
-            // {
-            //     minionButton.interactable = true;
-            // }            
+            if (bugs <= ogrePow * 10)
+            {
+                ogrePowButton.interactable = false;
+            }
+
+            if (bugs > 100 * minion + 100)
+            {
+                minionButton.interactable = true;
+            }            
         }
         
     }
@@ -131,6 +149,10 @@ public class ClickForScore : MonoBehaviour
             ogrePowText.text = "Ogre Power: " + ogrePow;
             minionText.text = "Minion: " + minion;
             villageText.text = "Village: " + village;
+            ogrePowButtText.text = "OGRE POWER \n" + "cost: " + ogrePow * 10 + " bugs";
+            minionButtonText.text = "MINION \n" + "cost: " + (100 * minion + 100) + " bugs";
+            
+
 
             ogrePowButton.interactable = false;
             minionButton.interactable = false;
