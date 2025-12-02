34a36de100d4ccf3adc7198cf63632a837a2e265
Chris Henderson <christopherboy06@gmail.com>
2025-12-02 15:34:45 -0600

    sound effects for buttons added

--- Assets/Scripts/BGMStuff.cs
+++ Assets/Scripts/BGMStuff.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using TMPro; // For the standard UI Dropdown
+    // using TMPro; // If using TextMeshPro Dropdown
 
-public class BGMStuff : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class BGMSuff : MonoBehaviour
     {
-        
-    }
+        public TMP_Dropdown myDropdown; // Assign this in the Inspector
+         
+        public void DropDown(int index)
+        {
+            
+            switch (index)
+            {
+                case 0:
+                //audio1
+                break;
+                case 1:
+                //audio2 
+                break;
+                case 2:
+                //nothing
+                break;
+            }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            
+        }
     }
-}
