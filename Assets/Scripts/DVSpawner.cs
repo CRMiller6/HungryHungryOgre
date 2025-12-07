475ac9d6c36150e710cf943d5a47b27d16c2f54e
Crow <crowmiller6@gmail.com>
2025-12-07 02:21:51 -0600

    Quick Time works + timer + start of DV BackGround
    I have the quick time event added (minus wrong inputs) and text telling you what button to press is working. Plus a visible timer for the level (it does not bring you back yet).

---
+++ Assets/Scripts/DVSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class DVSpawner : MonoBehaviour
+{
+
+    // public GameObject spawnBgPrefab;
+    // public Transform spawnBgPoint;
+    // public float bgSpawnDistance = 5f;
+    // private GameObject currBg;
+    // private Vector3 ogBgSpawn;
+
+    // void Start()
+    // {
+    //     ogBgSpawn = spawnBgPoint.position;
+    //     SpawnPrefab();
+    // }
+
+    // void Update()
+    // {
+    //     float distance = Vector3.Distance(currBg.transform.position, ogBgSpawn);
+
+    //     if (distance >= bgSpawnDistance)
+    //     {
+    //         SpawnPrefab();
+    //     }
+    // }
+
+    // void SpawnPrefab()
+    // {
+    //     currBg = Instantiate(currBg, ogBgSpawn, spawnBgPoint.rotation);
+    // }
+}
--- Assets/Scripts/DestryVillageScript.cs
+++ Assets/Scripts/DestryVillageScript.cs
@@ -7,14 +7,21 @@ public class DestryVillageScript : MonoBehaviour
 {
 
     public string[] Alphabet = new string[26] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    public float timer = 0;
+    public float quickTimer = 0;
     public float sceneTimer;
     private int three = 3;
     private float halfSec = 0.5f;
     public string quickLetter;
     public TMP_Text quickText;
+    public GameObject qText;
     public string playerInput;
     public float dVScore;
+    public GameObject timerGO;
+    public TMP_Text timerTxt;
+    public float timerVounter;
+    public float countDown = 60.0f;
+    private int one = 1;
+    public int letterRepeater = 0;
 
 
     
@@ -27,43 +34,78 @@ public class DestryVillageScript : MonoBehaviour
     void Update()
     {
         
-        if (timer <= 0)
+        timerVounter += Time.deltaTime;
+        if (timerVounter >= one)
         {
-            Debug.Log (timer);
+            countDown -= 1;
+            timerVounter -= 1;
+        }
+        
+        TextUpdate();
+
+        foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (keyCode >= KeyCode.A && keyCode <= KeyCode.Z)
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    playerInput = keyCode.ToString();
+                    Debug.Log("Player pressed: " + playerInput);
+                    //break; 
+                }
+            }
+        }
+
+
+
+        if (quickTimer >= -0.0007 && quickTimer <= 0.0007 && letterRepeater == 0)
+        {
+            //Debug.Log (quickTimer);
             quickLetter = Alphabet[UnityEngine.Random.Range(0, Alphabet.Length)];
             Debug.Log (quickLetter);
+            letterRepeater += 1;
         }
 
-        timer += Time.deltaTime;
+        quickTimer += Time.deltaTime;
         sceneTimer += Time.deltaTime;
 
-        if (timer < three && playerInput == quickLetter)
+        if (quickTimer < three && playerInput == quickLetter)
         {
             //win QT
-            timer = 0;
+            quickTimer = 0;
             dVScore += 0.05f;
+            Debug.Log ("did QT");
+            letterRepeater = 0;
         }
 
-        if (timer >= three && playerInput != quickLetter)
+        if (quickTimer >= three && playerInput != quickLetter)
         {
-            Debug.Log (timer);
+            //Debug.Log (quickTimer);
             //lose QT
-            timer = -0.5f;
-            Debug.Log (timer);
+            quickTimer = -1f;
+            quickLetter = null;
+            quickText.text = "WAIT penalty";
+            //Debug.Log (quickTimer);
             Debug.Log("lose quickTime");
+            letterRepeater = 0;
         }
 
-        if (sceneTimer > 60)
+        if (countDown <= 0)
         {
             //change back to clicker scene
         }
-
     }
 
     public void QuickTime()
     {
-        
 
-        
     }
+
+    public void TextUpdate()
+    {
+        timerTxt.text = "Time remaining: " + countDown;
+
+        quickText.text = "Click " + quickLetter + " to move";
+    }
+    
 }
