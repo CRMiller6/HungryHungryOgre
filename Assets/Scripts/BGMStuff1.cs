cb532c402a2d45dcc0ae8d8cc67b09501e94ba90
Chris Henderson <christopherboy06@gmail.com>
2025-12-10 21:35:09 -0600

    added the background sounds to main menu and clicker main, it also automatically cycles through them

---
+++ Assets/Scripts/BGMStuff1.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class BGMStuff : MonoBehaviour
+{
+    public AudioSource bgmSource;
+
+    public AudioClip trackA;
+    public AudioClip trackB;
+
+    private float timeA = 108f;
+    private float timeB = 62f;
+
+    private float timer;
+    private bool playingA = true;
+
+    void Start()
+    {
+        // start track A
+        PlayTrack(trackA);
+        timer = timeA;
+    }
+
+    void Update()
+    {
+        AutoSwitch();
+    }
+
+    void AutoSwitch()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            if (playingA)
+            {
+                PlayTrack(trackB);
+                timer = timeB;
+            }
+            else
+            {
+                PlayTrack(trackA);
+                timer = timeA;
+            }
+
+            playingA = !playingA;
+        }
+    }
+
+    void PlayTrack(AudioClip clip)
+    {
+        if (clip == null) return;
+
+        bgmSource.clip = clip;
+        bgmSource.Play();
+    }
+}
