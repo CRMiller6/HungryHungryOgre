76f7b8c8e997f147adfed420b3b1fcc55ed1a233
Crow <crowmiller6@gmail.com>
2025-12-10 22:01:24 -0600

    clean

--- Assets/Scripts/VDSingle.cs
+++ Assets/Scripts/VDSingle.cs
@@ -22,16 +22,4 @@ public class VDSingle : MonoBehaviour
         }
         DontDestroyOnLoad(this);
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
