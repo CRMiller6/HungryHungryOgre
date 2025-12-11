6635805c3d56b072c3343bd90a28e6833836fc38
Crow <crowmiller6@gmail.com>
2025-12-10 21:59:42 -0600

    Xander taught us Singletons

---
+++ Assets/Scripts/VDSingle.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class VDSingle : MonoBehaviour
+{
+    public static VDSingle instance;
+    public long destroyVillageTrack;
+
+    public static VDSingle GetInstance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        if(VDSingle.instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
